4174a8b601060ce91aa1154112ce353d4b166231
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-01 13:57:39 +0200

    feature(api): Metodes per afegir, actualitzar i eliminar afegits Closes #2

---
+++ ITBGameJam2025Api/DTOs/GameDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITBGameJam2025Api.DTOs
+{
+    public class GameDTO
+    {
+        [Required(ErrorMessage = "Aquest camp és obligatori")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Aquest camp és obligatori")]
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "Aquest camp és obligatori")]
+        public string? Developer { get; set; }
+        public string? Image { get; set; }
+    }
+}
