858ef0cfd8f9ab6d4913dddb41db270a90e950e2
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-01 13:26:14 +0200

    feature(api): Afegir metode get per jocs Closes #1

---
+++ ITBGameJam2025Api/Controllers/GamesController.cs
@@ -0,0 +1,29 @@
+﻿using ITBGameJam2025Api.Data;
+using ITBGameJam2025Api.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITBGameJam2025Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public GamesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
+        {
+            var games = await _context.Games.ToListAsync();
+            if (games.Count == 0)
+            {
+                return NotFound("There is no games");
+            }
+            return Ok(games);
+        }
+    }
+}
