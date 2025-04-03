8176187359c2d786a181002e2f484c1c97794e8a
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-03 11:36:43 +0200

    fetaure(DB): Clase pels vots dels jocs afegida

--- ITBGameJam2025Api/Data/AppDbContext.cs
+++ ITBGameJam2025Api/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace ITBGameJam2025Api.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Game> Games { get; set; }
+        public DbSet<GameVote> GameVotes { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
