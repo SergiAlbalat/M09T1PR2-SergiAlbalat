79f867e0e1a6fe9d1380af4ceaf3f57d0c1d5d2f
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-20 19:34:26 +0200

    feature(client): Chat funciona amb usuaris Closes #38

--- ITBGameJam2025Client/Pages/UsersXat.cshtml.cs
+++ ITBGameJam2025Client/Pages/UsersXat.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Headers;
+using System.Net.Http;
 
 namespace ITBGameJam2025Client.Pages
 {
     public class UsersXatModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _httpClient;
+
+        public bool LoggedIn { get; set; } = false;
+        public string UserName { get; set; }
+
+        public UsersXatModel(IHttpClientFactory httpClient)
         {
+            _httpClient = httpClient;
+        }
+
+        public async Task OnGet()
+        {
+            LoggedIn = Tools.TokenHelper.IsTokenSession(HttpContext.Session.GetString("AuthToken"));
+            if (!LoggedIn)
+            {
+                GoIndex();
+            }
+            else
+            {
+                var client = _httpClient.CreateClient("GamesApi");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("AuthToken"));
+                var response = await client.GetAsync("api/Auth/GetName");
+                UserName = await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private IActionResult GoIndex()
+        {
+            return RedirectToPage("index");
         }
     }
 }
