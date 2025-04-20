4c0ae2d11ac3297c839e466ba2ed1ea2211887fd
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-04-20 12:51:25 +0200

    feature(api): El controlador per fer i consultar les votacions dels jocs funciona correctament Closes #27

---
+++ ITBGameJam2025Api/Migrations/20250420100321_GameVoteUserIdTypeChange.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITBGameJam2025Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameVoteUserIdTypeChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "GameVotes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "GameVotes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
